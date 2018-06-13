82aa507548a305b9e0431af18e34fd4873b10fa2
mpjimenez <82.matthew.jimenez@gmail.com>
2018-06-12 17:09:43 -0700

    add test.csv that adds city_state column

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -1,10 +1,10 @@
 import sys
+import os
 import pandas as pd 
 import numpy as np
 import csv
-import geocoder
 import time
-
+import geocoder
 
 SHOOTINGS_DATASET = 'datasets/fatal-police-shootings-data.csv'
 COORDINATES_PATH = 'datasets/city_coordinates.csv'
@@ -44,23 +44,26 @@ def get_geo_coords(shootings_dataset):
     for city, state in zip(cities, states):
         city_states.append(city + ' ' + state)
 
-    #test = city_states[:3]
-    #print(test)
-    #coords = list(map(lambda x: geocoder.google(x), test))
-    coords = list(map(lambda x: geocoder.google(x), city_states))
+    
+    test = city_states[:3]
+    # Testing only 3 city_states
+    coords = geocoder.mapquest(test, method = 'batch', key = 'put_key_here')
+    #coords = geocoder.mapquest(city_states, method = 'batch', key = 'put_key_here')
 
     ndx = 0
     for location in coords:
+        #print(location.address, location.latlng)
         city_coordinates.update({city_states[ndx] : location.latlng})
         ndx += 1
-       
+
     results_file = csv.writer(open(COORDINATES_PATH, 'w'))
     results_file.writerow(['Location ', 'Lat', 'Lon'])
 
     for loc in city_coordinates:
         results_file.writerow([loc, city_coordinates[loc][0], city_coordinates[loc][1]])
-        #print(loc, city_coordinates[loc][0], city_coordinates[loc][1])
+        print(loc, city_coordinates[loc][0], city_coordinates[loc][1])
 
     end = time.time()
     print('It took {} seconds to get 3,399 city coordinates!'.format(end - start))
     sys.exit()
+    
